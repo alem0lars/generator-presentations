1037747c596ba0bf6671acf93adbfb905f32e09a
Alessandro Molari <molari.alessandro@gmail.com>
2015-03-26 22:59:11 +0100

    Updated template

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var generators = require("yeoman-generator");
 var _          = require("lodash");
+var _s         = require("underscore.string");
 
 
 module.exports = generators.Base.extend({
@@ -19,6 +20,18 @@ module.exports = generators.Base.extend({
       name: "pkgVers",
       message: "What version should we put in package.json file?",
       default: "0.0.1"
+    }, {
+      name: "repoUrl",
+      message: "What's the repository url?"
+    }, {
+      name: "authorName",
+      message: "What's the presentation author name?"
+    }, {
+      name: "authorEmail",
+      message: "What's the presentation author email?"
+    }, {
+      name: "authorUrl",
+      message: "What's the presentation author website?"
     }];
 
     this.prompt(prompts, function(props) {
@@ -28,8 +41,18 @@ module.exports = generators.Base.extend({
     }.bind(this));
   },
 
-  writing: {
-    // TODO
+  writing: function() {
+    this.template("package.json");
+    this.template("editorconfig", ".editorconfig");
+
+    this.remote("nextreamlabs", "lintersjam", function(err, remote) {
+      remote.copy(".", "jshintrc");
+    });
+
+    this.template("travis.yml", ".travis.yml");
+    this.template("README.md");
+    this.copy("gitattributes", ".gitattributes");
+    this.copy("gitignore", ".gitignore");
   },
 
   install: function() {
